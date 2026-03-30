ff1dfbfa75786990e46e20abc4c4567f2ad042b9
Abner <abnerrodriguesrolim@protonmail.com>
2026-03-30 13:59:34 -0300

    Adicionei o uso do switch para um menu interativo e comparar os atributos.

--- super_trunfo_c/super_trunfo.c
+++ super_trunfo_c/super_trunfo.c
@@ -22,7 +22,7 @@ int main(){
     scanf("%49s", nome1);
 
     printf("Digite a população do primeiro estado: ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
     
     printf("Digite a área do primeiro estado em km²: ");
     scanf("%f", &area1);
@@ -46,7 +46,7 @@ int main(){
     scanf("%49s", nome2);
 
     printf("Digite a população do segundo estado: ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
     
     printf("Digite a área do segundo estado: ");
     scanf("%f", &area2);
@@ -63,68 +63,51 @@ int main(){
     //Adição da densidade populacional e PIB per capita.
     float densidade1, densidade2;
     float pibcap1, pibcap2;
-    if (area1 != 0) //Evitar divisão por 0.
+    if (area1 != 0){ //Evitar divisão por 0.
         densidade1 = (float) populacao1 / area1;
-    else
+    }
+    else {
         densidade1 = 0;
+    }
     
-    if (area2 != 0)
+    if (area2 != 0){
         densidade2 = (float) populacao2 / area2;
-    else
+    }
+    else {
         densidade2 = 0;
+    }
     
-    if (populacao1 != 0) //Evitar divisão por 0 e multiplicar por um bilhão. Por que o PIB está em medida de bilhão, logo o PIB per capita iria sair arredondado para 0.
+    if (populacao1 != 0) { //Evitar divisão por 0 e multiplicar por um bilhão. Por que o PIB está em medida de bilhão, logo o PIB per capita iria sair arredondado para 0.
         pibcap1 = (pib1 * 1000000000.0) / (float) populacao1;
-    else
+    }
+    else {
         pibcap1 = 0;
+    }
     
-    if (populacao2 != 0)
+    if (populacao2 != 0){
         pibcap2 = (pib2 * 1000000000.0) / (float) populacao2;
-    else
+    }
+    else {
         pibcap2 = 0;
-    
+    }
+
     //Super Poder:
 
     float superpoder1, superpoder2;
-    if (densidade1 != 0)
-            superpoder1 = (float) pibcap1 + (float) p_turisticos1 + (float) pib1 + (float) area1 + (float) populacao1 + (1.0f/densidade1);
-        else
-            superpoder1 = (float) pibcap1 + (float) p_turisticos1 + (float) pib1 + (float) area1 + (float) populacao1; //Se a densidade for 0, não é possível calcular o inverso, então o super poder é calculado sem o inverso da densidade.
-    if (densidade2 != 0)
+    if (densidade1 != 0){
+        superpoder1 = (float) pibcap1 + (float) p_turisticos1 + (float) pib1 + (float) area1 + (float) populacao1 + (1.0f/densidade1);
+    }
+    else {
+        superpoder1 = (float) pibcap1 + (float) p_turisticos1 + (float) pib1 + (float) area1 + (float) populacao1;
+    }
+    if (densidade2 != 0){
         superpoder2 = (float) pibcap2 + (float) p_turisticos2 + (float) pib2 + (float) area2 + (float) populacao2 + (1.0f/densidade2);
-    else
-        superpoder2 = (float) pibcap2 + (float) p_turisticos2 + (float) pib2 + (float) area2 + (float) populacao2; //Se a densidade for 0, não é possível calcular o inverso, então o super poder é calculado sem o inverso da densidade.
+    }
+    else{
+       superpoder2 = (float) pibcap2 + (float) p_turisticos2 + (float) pib2 + (float) area2 + (float) populacao2; //Se a densidade for 0, não é possível calcular o inverso, então o super poder é calculado sem o inverso da densidade.
+        }
 
-    //Informações da primeira carta:
-    printf("Carta 1: \n"); 
-    printf("Estado: %c\n", estado1);
-    printf("Código: %s\n", codigo1);
-    printf("Nome: %s\n", nome1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Pontos turísticos: %d\n", p_turisticos1);
-    printf("Densidade populacional: %.2f\n", densidade1);
-    printf("PIB per capita: %.2f\n", pibcap1);
 
-    printf("\n"); //Pular linha.
-    
-    //Informações da segunda carta:
-    printf("Carta 2: \n"); 
-    printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
-    printf("Nome: %s\n", nome2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Pontos turísticos: %d\n", p_turisticos2);
-    printf("Densidade populacional: %.2f\n", densidade2);
-    printf("PIB per capita: %.2f\n", pibcap2);
-
-    printf("\n"); //Pular linha.
-
-    //Comparação dos atributos:
-    printf("Comparação das cartas (1 = carta 1 vence, 0 = carta 2 vence):\n");
     int resultado_populacao = populacao1 > populacao2;
     int resultado_area = area1 > area2;
     int resultado_pib = pib1 > pib2;
@@ -133,14 +116,106 @@ int main(){
     int resultado_pibcap = pibcap1 > pibcap2;
     int resultado_superpoder = superpoder1 > superpoder2;
 
-    printf("População: %d\n", resultado_populacao);
-    printf("Área: %d\n", resultado_area);
-    printf("PIB: %d\n", resultado_pib);
-    printf("Pontos turísticos: %d\n", resultado_p_turisticos);
-    printf("Densidade populacional: %d\n", resultado_densidade);
-    printf("PIB per capita: %d\n", resultado_pibcap);
-    printf("Super poder: %d\n", resultado_superpoder);
 
+    int opcao;
+    printf("Escolha uma opção: \n");
+    printf("1 - Atributos \n");
+    printf("2 - Comparação \n");
+    printf("Digite a opção desejada: ");
+    scanf(" %d", &opcao);
+    int atributo;
+    switch (opcao) {
+        case 1:
+            //Informações das cartas.
+            printf("Informações das cartas:\n");
+            printf("Carta 1: \n"); 
+            printf("Estado: %c\n", estado1);
+            printf("Código: %s\n", codigo1);
+            printf("Nome: %s\n", nome1);
+            printf("População: %lu\n", populacao1);
+            printf("Área: %.2f km²\n", area1);
+            printf("PIB: %.2f bilhões de reais\n", pib1);
+            printf("Pontos turísticos: %d\n", p_turisticos1);
+            printf("Densidade populacional: %.2f\n", densidade1);
+            printf("PIB per capita: %.2f\n", pibcap1);
+
+            printf("\n"); //Pular linha.
+            
+            printf("Carta 2: \n"); 
+            printf("Estado: %c\n", estado2);
+            printf("Código: %s\n", codigo2);
+            printf("Nome: %s\n", nome2);
+            printf("População: %lu\n", populacao2);
+            printf("Área: %.2f km²\n", area2);
+            printf("PIB: %.2f bilhões de reais\n", pib2);
+            printf("Pontos turísticos: %d\n", p_turisticos2);
+            printf("Densidade populacional: %.2f\n", densidade2);
+            printf("PIB per capita: %.2f\n", pibcap2);
+            break;
+        case 2:
+            //Comparação dos atributos.
+            printf("\n");
+            printf("Escolha um atributo para comparar (1 - Carta 1 Vence|0 - Carta 2 Vence): \n");
+            printf("1 - População \n");
+            printf("2 - Área \n");
+            printf("3 - PIB \n");
+            printf("4 - Pontos turísticos \n");
+            printf("5 - Densidade populacional \n");
+            printf("6 - PIB per capita \n");
+            printf("7 - Super poder \n");
+            printf("8 - Todos os atributos: \n");
+            printf("Digite a opção desejada: ");
+            break;
+        default:
+            printf("Opção inválida.\n");
+            break;
+    }
+        scanf(" %d", &atributo);
+            switch (atributo) {
+                case 1:
+                    printf("\n");
+                    printf("População: %d\n", resultado_populacao);
+                    break;
+                case 2:
+                    printf("\n");
+                    printf("Área: %d\n", resultado_area);
+                    break;
+                case 3:
+                    printf("\n");
+                    printf("PIB: %d\n", resultado_pib);
+                    break;
+                case 4:
+                    printf("\n");
+                    printf("Pontos turísticos: %d\n", resultado_p_turisticos);
+                    break;
+                case 5:
+                    printf("\n");
+                    printf("Densidade populacional: %d\n", resultado_densidade);
+                    break;
+                case 6:
+                    printf("\n");   
+                    printf("PIB per capita: %d\n", resultado_pibcap);
+                    break;
+                case 7:
+                    printf("\n");
+                    printf("Super poder: %d\n", resultado_superpoder);
+                    break;
+                case 8:
+                    printf("\n");
+                    printf("População: %d\n", resultado_populacao);
+                    printf("Área: %d\n", resultado_area);
+                    printf("PIB: %d\n", resultado_pib);
+                    printf("Pontos turísticos: %d\n", resultado_p_turisticos);
+                    printf("Densidade populacional: %d\n", resultado_densidade);
+                    printf("PIB per capita: %d\n", resultado_pibcap);
+                    printf("Super poder: %d\n", resultado_superpoder);
+                    break;
+            default:
+                printf("Opção inválida.\n");
+        }
+
+
+    printf("\n"); //Pular linha.
 
 
 
